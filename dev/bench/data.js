57b3e5a2ec0e2a521b66599b6138d9d3d3930b7b
github-action-benchmark <github@users.noreply.github.com>
2022-06-13 14:54:53 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for aa8bb5630550885091256d4389c9d6a3310b1b46

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655131524400,
+  "lastUpdate": 1655132093423,
   "repoUrl": "https://github.com/mascodeg/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -72,6 +72,44 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.995316416340802,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "brieuc.francois@cern.ch",
+            "name": "Brieuc Francois",
+            "username": "BrieucF"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa8bb5630550885091256d4389c9d6a3310b1b46",
+          "message": "Fix (#182)",
+          "timestamp": "2022-06-01T15:36:04+02:00",
+          "tree_id": "e88d4644ec18378f62c5a874865b6bc3c7b8fe1a",
+          "url": "https://github.com/mascodeg/FCCAnalyses/commit/aa8bb5630550885091256d4389c9d6a3310b1b46"
+        },
+        "date": 1655132092470,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.58730149269104,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.519476652145386,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
