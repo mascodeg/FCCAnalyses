7a4a46644561e080d8c5f4ceee975036d582ede0
github-action-benchmark <github@users.noreply.github.com>
2022-06-13 14:45:24 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for aa8bb5630550885091256d4389c9d6a3310b1b46

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655131522583,
+  "lastUpdate": 1655131524400,
   "repoUrl": "https://github.com/mascodeg/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,40 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "brieuc.francois@cern.ch",
+            "name": "Brieuc Francois",
+            "username": "BrieucF"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa8bb5630550885091256d4389c9d6a3310b1b46",
+          "message": "Fix (#182)",
+          "timestamp": "2022-06-01T15:36:04+02:00",
+          "tree_id": "e88d4644ec18378f62c5a874865b6bc3c7b8fe1a",
+          "url": "https://github.com/mascodeg/FCCAnalyses/commit/aa8bb5630550885091256d4389c9d6a3310b1b46"
+        },
+        "date": 1655131523808,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.580472477171084,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.995316416340802,
+            "unit": "Evt/s"
           }
         ]
       }
